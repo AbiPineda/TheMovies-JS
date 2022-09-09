65d0eeb56a9e87cf1e90fa0dddf990ee243454aa
Abigail <pineverdi@gmail.com>
2022-09-09 10:34:09 -0600

    migracion a axios

--- src/secrets.js
+++ src/secrets.js
@@ -1,1 +1,13 @@
-const API_KEY ='sdsdsd'
+const API_KEY ='275d17545576135364b8fc2cfdcc1c7c';
+
+/*
+API Key (v3 auth)
+275d17545576135364b8fc2cfdcc1c7c
+
+Example API Request
+https://api.themoviedb.org/3/movie/550?api_key=275d17545576135364b8fc2cfdcc1c7c
+
+API Read Access Token (v4 auth)
+eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNzVkMTc1NDU1NzYxMzUzNjRiOGZjMmNmZGNjMWM3YyIsInN1YiI6IjYzMWEyMGYzODgwYzkyMDA5ODdlYWQwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4vxwBHEByl11iOn_r9fjhSqaZO5wt4_pUxMpmXY_xpo
+
+*/
